1880d86d0381da264e04eeb393d49a1b0d222343
Phinocio <github@phinocio.com>
2023-07-02 20:10:50 -0600

    make clean every minute to test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('backup:clean')
-            ->daily()->at('01:00')
+            ->everyMinute()
             ->environments(['production'])
             ->onSuccess(function () {
                 Log::channel('backups')->info('✅ Clean Backups');
