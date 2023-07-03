0df8e8dacfaabdb415f707b904d35440db6bb093
Phinocio <github@phinocio.com>
2023-07-02 20:40:05 -0600

    make time correct

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('backup:clean')
-            ->everyMinute()
+            ->daily()->at('01:00')
             ->environments(['production'])
             ->onSuccess(function () {
                 Log::channel('backups')->info('✅ Clean Backups');
